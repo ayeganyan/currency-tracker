2fc8a54f65d1cdb634958b896ae6f30d07a8532c
Armen Yeganyan <ayeganyan92@gmail.com>
2020-07-14 12:01:27 +0400

    Fixed url paths

--- src/main/java/com/ayeganyan/currencytracker/conf/Urls.java
+++ src/main/java/com/ayeganyan/currencytracker/conf/Urls.java
@@ -2,6 +2,6 @@ package com.ayeganyan.currencytracker.conf;
 
 public class Urls {
     public static final String SWAGGER = "/swagger-ui.html";
-    public static final String SIGNUP = "/v1/user/signup";
-    public static final String AUTHENTICATE = "/v1/user/authenticate";
+    public static final String SIGNUP = "/v1/auth/signup";
+    public static final String AUTHENTICATE = "/v1/auth/authenticate";
 }
